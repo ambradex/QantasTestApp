cd9522d7b164174a32ba42988c4aafade7b1b1da
Dexter Anthony Ambrad <dexter.a.m.ambrad@accenture.com>
2025-10-20 18:03:28 +0800

    Initial Commit

---
+++ QantasTestApp/QantasTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QantasTestAppApp.swift
+//  QantasTestApp
+//
+//  Created by dexter.a.m.ambrad on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct QantasTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QantasTestAppTests/QantasTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  QantasTestAppTests.swift
+//  QantasTestAppTests
+//
+//  Created by dexter.a.m.ambrad on 10/20/25.
+//
+
+import Testing
+@testable import QantasTestApp
+
+struct QantasTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
